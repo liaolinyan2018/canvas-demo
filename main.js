0e2226357e602901dc585c0318a6415370e112ac
liaolinyan2018 <linyanliao@126.com>
2018-11-24 19:23:10 +0800

    触屏设备兼容

--- main.js
+++ main.js
@@ -19,7 +19,8 @@ brush.onclick = function(){
   tool.className = 'tool';
 }
 
-listenToMouse();
+listenToUser();
+
 
 /*************自定义函数工具************/
 
@@ -54,23 +55,29 @@ function autoSetCanvasSize(){
     setCanvasSize(canvas);
   }
 }
-/*监听鼠标动作-不需参*/
-function listenToMouse(){
+
+/*特性检测-是否支持触屏*/ 
+function listenToUser(){
+if(document.body.ontouchstart !== undefined){
+  //触屏设备-监听触屏动作
+
   //1.0按下鼠标
-  canvas.onmousedown = function(xxx){
-   using = true;
-   var x = xxx.clientX; //(clientX,clientY)是相对于视口的坐标
-   var y = xxx.clientY;
-   if(!usingEraser){
-       lastDot = {"x":x,"y":y} //把鼠标按下的点存到全局变量里了,哪里都能用
-   } else{
-       context.clearRect(x-5,y-5,10,10);//橡皮的大小规模
-   }
-}
+  canvas.ontouchstart = function(xxx){
+    using = true;
+    var x = xxx.touches[0].clientX; //(clientX,clientY)是相对于视口的坐标
+    var y = xxx.touches[0].clientY;
+    console.log(xxx);
+    if(!usingEraser){
+        lastDot = {"x":x,"y":y} //把鼠标按下的点存到全局变量里了,哪里都能用
+     } else{
+        context.clearRect(x-5,y-5,10,10);//橡皮的大小规模
+       }
+  }
+
   //2.0移动鼠标
-  canvas.onmousemove = function(xxx){
-    var x = xxx.clientX;
-    var y = xxx.clientY;
+  canvas.ontouchmove = function(xxx){
+    var x = xxx.touches[0].clientX;
+    var y = xxx.touches[0].clientY;
     if(using){
         if(!usingEraser){
             var newDot = {"x" : x ,"y": y }
@@ -82,12 +89,50 @@ function listenToMouse(){
     }else{
         return
     }
-}
-  
-//3.0松开鼠标
-  canvas.onmouseup = function(){
+  }
+
+  //3.0松开鼠标
+  canvas.ontouchend = function(){
     using = false;
   }
+
+}else{
+  //非触屏设备-监听鼠标动作
+
+  //1.0按下鼠标
+  canvas.onmousedown = function(xxx){
+    using = true;
+    var x = xxx.clientX; //(clientX,clientY)是相对于视口的坐标
+    var y = xxx.clientY;
+    if(!usingEraser){
+        lastDot = {"x":x,"y":y} //把鼠标按下的点存到全局变量里了,哪里都能用
+     } else{
+        context.clearRect(x-5,y-5,10,10);//橡皮的大小规模
+       }
+    }
+   //2.0移动鼠标
+   canvas.onmousemove = function(xxx){
+     var x = xxx.clientX;
+     var y = xxx.clientY;
+     if(using){
+         if(!usingEraser){
+             var newDot = {"x" : x ,"y": y }
+             drawLine(lastDot.x,lastDot.y,newDot.x,newDot.y);
+             lastDot = newDot;
+         }else{
+             context.clearRect(x-5,y-5,10,10);//橡皮的大小
+             }
+     }else{
+         return
+     }
+ }
+   
+ //3.0松开鼠标
+   canvas.onmouseup = function(){
+     using = false;
+   }
+}
+
 }
 
 
